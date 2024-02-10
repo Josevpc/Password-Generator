c2dc78357cd69835e2894bf83c9c15b620e98b2e
Josevpc <josevictorpiccin@gmail.com>
2024-02-10 17:48:00 -0300

    List :smiley:
    Add the list :)

--- main.py
+++ main.py
@@ -9,27 +9,31 @@ print('Welcome to the most advanced password generator!!!!')
 
 # Input of Data
 password_amount = int(
-    input('Please enter the number of passwords you wish to generate: '))
-password_length = int(
-    input('Please enter the desired length of the passwords: '))
+    input('\nPlease enter the number of passwords you wish to generate: '))
 
-# Code
-
-# I generate this string with chatgpt :)
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[];:,<.>/?|0123456789'
-
-for pwd in range(password_amount):
-    password = ''
-    for c in range(password_length):
-        password += random.choice(chars)
-    # include in list code
-
-# Output of Data
 if password_amount == 0:
     print('You dont want any password? Get out of here!')
-elif password_amount == 1:
-    print(f'Here are your password: {password[0]}')
 else:
-    print(f'Here are your {password_amount} passwords: ')
-    for x in password:
-        print(f'Password: {x}')
+    password_length = int(
+        input('Please enter the desired length of the passwords: '))
+
+    password_list = []
+
+    # Code
+    # I generate this string with chatgpt :)
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[];:,<.>/?|0123456789'
+
+    for pwd in range(password_amount):
+        password = ''
+        for c in range(password_length):
+            password += random.choice(chars)
+        # include in list code
+        password_list.append(password)
+
+    # Output of Data
+    if password_amount == 1:
+        print(f'Here are your password: {password_list[0]}')
+    else:
+        print(f'Here are your {password_amount} passwords: ')
+        for x in password_list:
+            print(x)
