3cc319b9bcbe2be1d392a8cafc5ed1420f706aeb
Josevpc <josevictorpiccin@gmail.com>
2024-02-17 14:35:51 -0300

    Finished :sunglasses:
    It's working :)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 I took the idea from a video by freeCodeCamp.org - Six Quick Python Projects, but I want to develop this project using lists. 
 Furthermore, I want to challenge myself by generating a password with at least 20% of its length consisting of random numbers 
-in random positions and 30% with random special characters also in random positions.
+in random positions and 30% with random symbols also in random positions.
 """
 import random
 
@@ -20,13 +20,51 @@ else:
     password_list = []
 
     # Code
-    # I generate this string with chatgpt :)
-    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[];:,<.>/?|0123456789'
+    # I generate this strings with chatgpt :)
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    numbers = '0123456789'
+    symbols = '!@#$%^&*()_+-=[];:,<.>/?|'
 
+    # Distribution of password characters
+    chars_length = round(password_length * 0.5)
+    numbers_length = round(password_length * 0.2)
+    symbols_length = round(password_length * 0.3)
+
+    # Normalization
+    distribution_length = chars_length + numbers_length + symbols_length
+
+    if distribution_length > password_length:
+        chars_length = chars_length - (distribution_length - password_length)
+
+    # Passwords Generator
     for pwd in range(password_amount):
         password = ''
+
+        distribution_list = ['chars', 'numbers', 'symbols']
+
+        chars_control = chars_length
+        numbers_control = numbers_length
+        symbols_control = symbols_length
+
         for c in range(password_length):
-            password += random.choice(chars)
+            distribution_choice = random.choice(distribution_list)
+            if distribution_choice == 'chars':
+                password += random.choice(chars)
+                chars_control -= 1
+                if chars_control == 0:
+                    distribution_list.remove('chars')
+
+            if distribution_choice == 'numbers':
+                password += random.choice(numbers)
+                numbers_control -= 1
+                if numbers_control == 0:
+                    distribution_list.remove('numbers')
+
+            if distribution_choice == 'symbols':
+                password += random.choice(symbols)
+                symbols_control -= 1
+                if symbols_control == 0:
+                    distribution_list.remove('symbols')
         # include in list code
         password_list.append(password)
 
